e655c64a36a28160ae8221658cac5b63a5a0dd51
akash1047 <94453657+akash1047@users.noreply.github.com>
2023-05-25 04:04:17 +0530

    shader::add_source() added

--- include/opengl/shader/shader.hpp
+++ include/opengl/shader/shader.hpp
@@ -1,16 +1,35 @@
 #pragma once
 
 #include "object.hpp"
+#include <exception>
 #include <filesystem>
 #include <glad/gl.h>
+#include <string>
 
 namespace opengl {
 namespace shader {
 
 class shader: public object {
+
 public:
-    class null_source {};
-    class syntax_error {};
+    class null_source: public std::exception {
+    public:
+        const char *what() const noexcept override {
+            return "null pointer is passed as shader source";
+        }
+    };
+
+    class syntax_error: public std::exception {
+    public:
+        syntax_error(std::string msg) : error_msg(std::move(msg)) {}
+
+        const char *what() const noexcept override {
+            return error_msg.c_str();
+        }
+
+    private:
+        std::string error_msg;
+    };
 
     shader(int shader_kind)
         : object(glCreateShader(shader_kind), glDeleteShader) {}
@@ -35,7 +54,14 @@ public:
 
     /* query end */
 
-    void add_file_source(std::filesystem::path file_path);
+    inline std::string info_log() {
+        int len = this->log_length() - 1;
+        std::string log(len, '\0');
+        glGetShaderInfoLog(this->get_id(), len, nullptr, log.data());
+        return log;
+    }
+
+    std::string add_file_source(std::filesystem::path file_path);
 
     inline void add_source(const char *source) {
         if (source == nullptr) throw null_source{};
@@ -44,7 +70,7 @@ public:
 
     inline void compile() {
         glCompileShader(this->get_id());
-        if (!this->is_compiled()) throw syntax_error{};
+        if (!this->is_compiled()) throw syntax_error{this->info_log()};
     }
 };
 
--- src/lib.cpp
+++ src/lib.cpp
@@ -1,1 +1,7 @@
-// opengl-cpp
+#include "../include/opengl/shader/shader.hpp"
+#include <fstream>
+
+std::string opengl::shader::shader::add_file_source(std::filesystem::path file_path) {
+    std::ifstream file(file_path);
+    return "";
+}
